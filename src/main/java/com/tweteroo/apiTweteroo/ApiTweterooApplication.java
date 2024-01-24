e933423569181d201760fe5f646e89199fe7cd2e
lbatista <lucasbatista2408@gmail.com>
2024-01-24 16:23:52 -0300

    repo initialized and connected to db

---
+++ src/main/java/com/tweteroo/apiTweteroo/ApiTweterooApplication.java
@@ -0,0 +1,13 @@
+package com.tweteroo.apiTweteroo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTweterooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTweterooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tweteroo/apiTweteroo/ApiTweterooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tweteroo.apiTweteroo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTweterooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
